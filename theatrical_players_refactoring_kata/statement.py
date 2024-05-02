4ec84d7c377efb52a52a0f62550f0519d9649433
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2024-05-03 00:16:45 +0200

    r extract method

--- theatrical_players_refactoring_kata/statement.py
+++ theatrical_players_refactoring_kata/statement.py
@@ -22,11 +22,9 @@ def statement(invoice, plays):
         else:
             raise ValueError(f'unknown type: {play["type"]}')
 
-        # add volume credits
-        volume_credits += max(perf['audience'] - 30, 0)
-        # add extra credit for every ten comedy attendees
-        if "comedy" == play["type"]:
-            volume_credits += math.floor(perf['audience'] / 5)
+        volume_credits += calculate_performance_volume_credits(
+            perf['audience'], play["type"])
+
         # print line for this order
         amount = this_amount/100
         entries += f' {play["name"]}: {f"${amount :0,.2f}"} ({perf["audience"]} seats)\n'
@@ -36,5 +34,17 @@ def statement(invoice, plays):
     owed = f'Amount owed is {f"${amount1 :0,.2f}"}\n'
     earned = f'You earned {volume_credits} credits\n'
     return header + entries + owed + earned
+
+
+def calculate_performance_volume_credits(perf_audience, play_type):
+    # add volume credits
+    audience_volume_credits = max(perf_audience - 30, 0)
+    # add extra credit for every ten comedy attendees
+    comedy_volume_credits = 0
+    if "comedy" == play_type:
+        comedy_volume_credits = math.floor(perf_audience / 5)
+    performance_volume_credits = (
+            audience_volume_credits + comedy_volume_credits)
+    return performance_volume_credits
 
 
