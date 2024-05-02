75fbef793665fe23c0885ffb313122913694dc11
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2024-05-02 23:45:09 +0200

    R extracted method

--- theatrical_players_refactoring_kata/statement.py
+++ theatrical_players_refactoring_kata/statement.py
@@ -7,34 +7,40 @@ def statement(invoice, plays):
     result = f'Statement for {invoice["customer"]}\n'
 
     for perf in invoice['performances']:
-        play = plays[perf['playID']]
-        if play['type'] == "tragedy":
-            this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
-        elif play['type'] == "comedy":
-            this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
-
-            this_amount += 300 * perf['audience']
-
-        else:
-            raise ValueError(f'unknown type: {play["type"]}')
-
-        # add volume credits
-        volume_credits += max(perf['audience'] - 30, 0)
-        # add extra credit for every ten comedy attendees
-        if "comedy" == play["type"]:
-            volume_credits += math.floor(perf['audience'] / 5)
-        # print line for this order
-        amount = this_amount/100
-        result += f' {play["name"]}: {f"${amount :0,.2f}"} ({perf["audience"]} seats)\n'
-        total_amount += this_amount
+        result, total_amount, volume_credits = apple_sauce(perf, plays, result,
+                                                           total_amount,
+                                                           volume_credits)
 
     amount1 = total_amount/100
     result += f'Amount owed is {f"${amount1 :0,.2f}"}\n'
     result += f'You earned {volume_credits} credits\n'
     return result
+
+
+def apple_sauce(perf, plays, result, total_amount, volume_credits):
+    play = plays[perf['playID']]
+    if play['type'] == "tragedy":
+        this_amount = 40000
+        if perf['audience'] > 30:
+            this_amount += 1000 * (perf['audience'] - 30)
+    elif play['type'] == "comedy":
+        this_amount = 30000
+        if perf['audience'] > 20:
+            this_amount += 10000 + 500 * (perf['audience'] - 20)
+
+        this_amount += 300 * perf['audience']
+
+    else:
+        raise ValueError(f'unknown type: {play["type"]}')
+    # add volume credits
+    volume_credits += max(perf['audience'] - 30, 0)
+    # add extra credit for every ten comedy attendees
+    if "comedy" == play["type"]:
+        volume_credits += math.floor(perf['audience'] / 5)
+    # print line for this order
+    amount = this_amount / 100
+    result += f' {play["name"]}: {f"${amount :0,.2f}"} ({perf["audience"]} seats)\n'
+    total_amount += this_amount
+    return result, total_amount, volume_credits
 
 
