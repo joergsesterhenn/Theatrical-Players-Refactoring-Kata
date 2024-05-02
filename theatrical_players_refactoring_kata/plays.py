fd3d124cde142b0896fa2abaacebc1d09e25c14d
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2024-05-03 01:51:37 +0200

    introduced polymorphism

---
+++ theatrical_players_refactoring_kata/plays.py
@@ -0,0 +1,59 @@
+import math
+from typing import Self
+from abc import ABCMeta, abstractmethod
+
+
+class Play(metaclass=ABCMeta):
+    def __init__(self, play_type, play_name):
+        self.play_type = play_type
+        self.play_name = play_name
+
+    @abstractmethod
+    def calculate_amount(self, audience):
+        pass
+
+    @abstractmethod
+    def calculate_credits(self, audience):
+        pass
+
+    @classmethod
+    def of(cls, play_type, play_name) -> Self:
+        if "comedy" == play_type:
+            return ComedyPlay(play_type, play_name)
+        elif "tragedy" == play_type:
+            return TragedyPlay(play_type, play_name)
+        else:
+            raise ValueError(f'unknown type: {play_type}')
+
+
+class ComedyPlay(Play):
+
+    def calculate_amount(self, audience):
+        this_amount = 30000
+        if audience > 20:
+            this_amount += 10000 + 500 * (audience - 20)
+
+        this_amount += 300 * audience
+        return this_amount
+
+    def calculate_credits(self, audience):
+        # add volume credits
+        audience_volume_credits = max(audience - 30, 0)
+        # add extra credit for every ten comedy attendees
+        comedy_volume_credits = math.floor(audience / 5)
+        return audience_volume_credits + comedy_volume_credits
+
+
+class TragedyPlay(Play):
+    def calculate_amount(self, audience):
+        this_amount = 40000
+        if audience > 30:
+            this_amount += 1000 * (audience - 30)
+
+        return this_amount
+
+    def calculate_credits(self, audience):
+        # add volume credits
+        audience_volume_credits = max(audience - 30, 0)
+        # add extra credit for every ten comedy attendees
+        return audience_volume_credits
--- theatrical_players_refactoring_kata/statement.py
+++ theatrical_players_refactoring_kata/statement.py
@@ -1,16 +1,18 @@
 import math
 
+from theatrical_players_refactoring_kata.plays import Play
+
 
 def statement(invoice, plays):
     total_amount = 0
     volume_credits = 0
     entries = ""
     for perf in invoice['performances']:
-        play = plays[perf['playID']]
-        amount = calculate_amount(perf['audience'], play['type'])
-        volume_credits += calculate_volume_credits(perf['audience'],
-                                                   play['type'])
-        entries += format_entry(amount, perf['audience'], play["name"])
+        play_entry = plays[perf['playID']]
+        play = Play.of(play_entry['type'], play_entry['name'])
+        amount = play.calculate_amount(perf['audience'])
+        volume_credits += play.calculate_credits(perf['audience'])
+        entries += format_entry(amount, perf['audience'], play_entry["name"])
         total_amount += amount
 
     return format_header(invoice) + entries + format_owed(
@@ -32,29 +34,3 @@ def format_header(invoice):
 def format_entry(amount, audience, play_name):
     return f' {play_name}: {f"${amount / 100 :0,.2f}"} ({audience} seats)\n'
 
-
-def calculate_amount(audience, play_type):
-    if play_type == "tragedy":
-        this_amount = 40000
-        if audience > 30:
-            this_amount += 1000 * (audience - 30)
-    elif play_type == "comedy":
-        this_amount = 30000
-        if audience > 20:
-            this_amount += 10000 + 500 * (audience - 20)
-
-        this_amount += 300 * audience
-
-    else:
-        raise ValueError(f'unknown type: {play_type}')
-    return this_amount
-
-
-def calculate_volume_credits(perf_audience, play_type):
-    # add volume credits
-    audience_volume_credits = max(perf_audience - 30, 0)
-    # add extra credit for every ten comedy attendees
-    comedy_volume_credits = 0
-    if "comedy" == play_type:
-        comedy_volume_credits = math.floor(perf_audience / 5)
-    return audience_volume_credits + comedy_volume_credits
