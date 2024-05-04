3f2009daa13016fe992a8f99bdc65ca1d639bf9d
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2024-05-04 23:59:26 +0200

    cleanup

--- tests/test_statement.py
+++ tests/test_statement.py
@@ -1,6 +1,5 @@
 import json
 
-import pytest
 from approval_utilities.utils import get_adjacent_file
 from approvaltests import verify
 from theatrical_players_refactoring_kata.statement import statement
