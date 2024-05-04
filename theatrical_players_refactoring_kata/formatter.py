9a9f6cb70ac59afac50ccd9ecd9b44a148e3f277
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2024-05-04 23:36:17 +0200

    moved formatter to separate file

---
+++ theatrical_players_refactoring_kata/formatter.py
@@ -0,0 +1,32 @@
+def format_statement(entries, invoice, amount, volume_credits,
+                     statement_type="printout"):
+    if statement_type == "printout":
+        return (f'Statement for {invoice["customer"]}\n'
+                + format_printout_entries(entries)
+                + f'Amount owed is {f"${amount :0,.2f}"}\n'
+                + f'You earned {volume_credits} credits\n')
+    elif statement_type == "html":
+        return (f'<!DOCTYPE html>\n<html>\n<body>\n<h1>Statement for {
+                invoice["customer"]}</h1>\n'
+                + format_html_entries(entries)
+                + f'<p>Amount owed is {f"${amount :0,.2f}"}</p>\n'
+                + f'<p>You earned {volume_credits
+                                   } credits</p>\n</body>\n</html>\n')
+    else:
+        raise Exception("Unknown type of statement: " + statement_type)
+
+
+def format_printout_entries(entries):
+    return "".join(
+        [f' {entry.get("name")}: {f"${entry.get("amount") :0,.2f}"} ({
+         entry.get("audience")} seats)\n' for entry in entries]
+    )
+
+
+def format_html_entries(entries):
+    return "".join(
+        [f'<p style="margin-left: 20px;">{
+         entry.get("name")}: {f"${
+          entry.get("amount"):0,.2f}"} ({
+          entry.get("audience")} seats)</p>\n' for entry in entries]
+    )
--- theatrical_players_refactoring_kata/statement.py
+++ theatrical_players_refactoring_kata/statement.py
@@ -1,3 +1,4 @@
+from theatrical_players_refactoring_kata.formatter import format_statement
 from theatrical_players_refactoring_kata.plays_calculator import PlayCalculator
 
 
@@ -24,36 +25,3 @@ def calculate_statement_entries(invoice, plays):
         entries.append({'amount': amount, 'audience': perf['audience'],
                         'name': play_entry["name"], 'credits': volume_credits})
     return entries
-
-
-def format_statement(entries, invoice, amount, volume_credits,
-                     statement_type="printout"):
-    if statement_type == "printout":
-        return (f'Statement for {invoice["customer"]}\n'
-                + format_printout_entries(entries)
-                + f'Amount owed is {f"${amount :0,.2f}"}\n'
-                + f'You earned {volume_credits} credits\n')
-    else:
-        return (f'<!DOCTYPE html>\n<html>\n<body>\n<h1>Statement for {
-                invoice["customer"]}</h1>\n'
-                + format_html_entries(entries)
-                + f'<p>Amount owed is {f"${amount :0,.2f}"}</p>\n'
-                + f'<p>You earned {volume_credits
-                                   } credits</p>\n</body>\n</html>\n')
-
-
-def format_printout_entries(entries):
-    return "".join(
-        [f' {entry.get("name")}: {f"${entry.get("amount") :0,.2f}"} ({
-         entry.get("audience")} seats)\n' for entry in entries]
-    )
-
-
-def format_html_entries(entries):
-    return "".join(
-        [f'<p style="margin-left: 20px;">{entry.get("name")}: {f"${entry.get("amount") :0,.2f}"} ({
-         entry.get("audience")} seats)</p>\n' for entry in entries]
-    )
-
-
-
